2e4ed4ea921639dc6f9bb3f9ceeda2772580fca9
Johnny Proano <jproano22@yahoo.com>
2023-05-26 09:47:31 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Nav } from "./Nav";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,7 @@
+export function Nav() {
+  return (
+    <div className="nav">
+      <h2>my travel journal</h2>
+    </div>
+  );
+}
